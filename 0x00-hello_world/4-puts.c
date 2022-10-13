915788369dab184c4b767a48d8d0e7cb56d3be5a
OLANREWAJU IDOWU <send2lanre@yahoo.com>
2022-10-13 15:08:52 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print the string
+ * this program is to print "Programming is like building a multilingual puzzle"
+ * return: 0
+ */
+int main(void)
+{
+	put("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
