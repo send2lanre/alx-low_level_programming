f811389c5b4bd85eb321cb319972840c0a642fbe
OLANREWAJU IDOWU <send2lanre@yahoo.com>
2022-11-01 15:57:56 -0700

    Write a function that fills memory with a constant byte. - Prototype: char *_memset(char *s, char b, unsigned int n); - The _memset() function fills the first n bytes of the memory area pointed to by s with the constant byte b. -Returns a pointer to the memory area s. - FYI: The standard library provides a similar function: memset. Run man memset to learn more.

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * _memset - Fills the first n bytes of the memory area pointed to by s with 
- * the constant byte b, fills a buffer with a constant for a specified size
- * @s: This is the buffer to be filled 
+ * _memset - Fills the first n bytes of the memory area pointed to by s
+ * with the constant byte b, fills a buffer with a constant for a specified size
+ * @s: This is the buffer to be filled
  * @b: This is the char that will be used to fill buffer
- * @n: bytes of the memory
+ * @n: This is the no of bytes to fill in the memory array of s
  * Return: a pointer to the memory area (s)
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
